3952ad065c7cd5c6cdd70cbfbd9b47133e038d5e
Yulia S <startyupp@gmail.com>
2023-06-06 16:10:00 -0700

    finished implementing change colors for temp as we tap the buttons. functions as planned

--- src/index.js
+++ src/index.js
@@ -1,50 +1,42 @@
+const state = { temp: 75 };
 const currentTemp = document.getElementById('current-temp');
 
 const displayTemp = () => {
-  //how we update the displayed temp
+  currentTemp.textContent = `${state.temp}`;
 };
 
+const upButton = document.getElementById('up-temp');
+const downButton = document.getElementById('down-temp');
+
 const setUpTemp = () => {
-  const upButton = document.getElementsById('up-temp');
+  state.temp += 1;
+  displayTemp();
+  changeColor(state.temp);
 };
 
 const setDownTemp = () => {
-  const downButton = document.getElementsById('down-temp');
+  state.temp -= 1;
+  displayTemp();
+  changeColor(state.temp);
 };
 
-//expected behavior of currentTemp as it changes
-if (currentTemp < 50) {
-  currentTemp.setAttribute('class', 'teal');
-} //-> number and background color changes to Teal
-else if (currentTemp < 60) {
-  currentTemp.setAttribute('class', 'green');
-} //-> number and background color changes to Green
-else if (currentTemp < 70) {
-  currentTemp.setAttribute('class', 'yellow');
-} //-> number and background color changes to Yellow
-else if (currentTemp < 80) {
-  currentTemp.setAttribute('class', 'orange');
-} //-> number and background color changes to Orange
-else if (currentTemp >= 80) {
-  currentTemp.setAttribute('class', 'orange');
-}
+const changeColor = function (temprature) {
+  if (temprature < 50) {
+    currentTemp.setAttribute('class', 'teal'); //(currentTemp.style.color = 'teal')
+  } else if (temprature < 60) {
+    currentTemp.setAttribute('class', 'green');
+  } else if (temprature < 70) {
+    currentTemp.setAttribute('class', 'yellow');
+  } else if (temprature < 80) {
+    currentTemp.setAttribute('class', 'orange');
+  } else if (temprature >= 80) {
+    currentTemp.setAttribute('class', 'red');
+  }
+};
 
 const registerEventHandlers = () => {
-  // const addCommentButton = document.getElementById('guestbook__button');
   upButton.addEventListener('click', setUpTemp);
   downButton.addEventListener('click', setDownTemp);
-
-  //   document.getElementById('kudos__button').addEventListener('click', addKudos);
-
-  //   document
-  //     .getElementById('blue__button')
-  //     .addEventListener('click', setBlueTheme);
-
-  //   document.getElementById('red__button').addEventListener('click', setRedTheme);
-
-  //   document
-  //     .getElementById('green-button')
-  //     .addEventListener('click', setGreenTheme);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
